413f4913f03c20c5f76a9e4d6fd5f407b14d576c
Frank Chen <biohazard93011@ntu.im>
2021-01-20 10:47:52 +0800

    test json

--- app/models.py
+++ app/models.py
@@ -174,7 +174,7 @@ def handle_text_message(event):
                         weight='bold',
                         flex=1
                     ),
-                    TextComponent(text='工作經驗', weight='bold', color='#aaaaaa', size='lg'),
+                    TextComponent(text='工作經驗', weight='bold', color='#aaaaaa', size='lg', margin='xl'),
                     # work experience
                     BoxComponent(
                         layout='horizontal',
@@ -883,6 +883,327 @@ def handle_postback(event):
                 [   TextSendMessage(text=answer),
                     TextSendMessage(text='準確度評估為:{}'.format(response['accuracy']))
                 ]
+            )
+        elif(action == "contact"):
+            bubble_string = """
+            {
+                "type": "bubble",
+                "size": "mega",
+                "header": {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                    {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [
+                        {
+                            "type": "text",
+                            "text": "FROM",
+                            "color": "#ffffff66",
+                            "size": "sm"
+                        },
+                        {
+                            "type": "text",
+                            "text": "Akihabara",
+                            "color": "#ffffff",
+                            "size": "xl",
+                            "flex": 4,
+                            "weight": "bold"
+                        }
+                        ]
+                    },
+                    {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [
+                        {
+                            "type": "text",
+                            "text": "TO",
+                            "color": "#ffffff66",
+                            "size": "sm"
+                        },
+                        {
+                            "type": "text",
+                            "text": "Shinjuku",
+                            "color": "#ffffff",
+                            "size": "xl",
+                            "flex": 4,
+                            "weight": "bold"
+                        }
+                        ]
+                    }
+                    ],
+                    "paddingAll": "20px",
+                    "backgroundColor": "#0367D3",
+                    "spacing": "md",
+                    "height": "154px",
+                    "paddingTop": "22px"
+                },
+                "body": {
+                    "type": "box",
+                    "layout": "vertical",
+                    "contents": [
+                    {
+                        "type": "text",
+                        "text": "Total: 1 hour",
+                        "color": "#b7b7b7",
+                        "size": "xs"
+                    },
+                    {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "contents": [
+                        {
+                            "type": "text",
+                            "text": "20:30",
+                            "size": "sm",
+                            "gravity": "center"
+                        },
+                        {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                            {
+                                "type": "filler"
+                            },
+                            {
+                                "type": "box",
+                                "layout": "vertical",
+                                "contents": [],
+                                "cornerRadius": "30px",
+                                "height": "12px",
+                                "width": "12px",
+                                "borderColor": "#EF454D",
+                                "borderWidth": "2px"
+                            },
+                            {
+                                "type": "filler"
+                            }
+                            ],
+                            "flex": 0
+                        },
+                        {
+                            "type": "text",
+                            "text": "Akihabara",
+                            "gravity": "center",
+                            "flex": 4,
+                            "size": "sm"
+                        }
+                        ],
+                        "spacing": "lg",
+                        "cornerRadius": "30px",
+                        "margin": "xl"
+                    },
+                    {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "contents": [
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "contents": [
+                            {
+                                "type": "filler"
+                            }
+                            ],
+                            "flex": 1
+                        },
+                        {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                            {
+                                "type": "box",
+                                "layout": "horizontal",
+                                "contents": [
+                                {
+                                    "type": "filler"
+                                },
+                                {
+                                    "type": "box",
+                                    "layout": "vertical",
+                                    "contents": [],
+                                    "width": "2px",
+                                    "backgroundColor": "#B7B7B7"
+                                },
+                                {
+                                    "type": "filler"
+                                }
+                                ],
+                                "flex": 1
+                            }
+                            ],
+                            "width": "12px"
+                        },
+                        {
+                            "type": "text",
+                            "text": "Walk 4min",
+                            "gravity": "center",
+                            "flex": 4,
+                            "size": "xs",
+                            "color": "#8c8c8c"
+                        }
+                        ],
+                        "spacing": "lg",
+                        "height": "64px"
+                    },
+                    {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "contents": [
+                        {
+                            "type": "box",
+                            "layout": "horizontal",
+                            "contents": [
+                            {
+                                "type": "text",
+                                "text": "20:34",
+                                "gravity": "center",
+                                "size": "sm"
+                            }
+                            ],
+                            "flex": 1
+                        },
+                        {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                            {
+                                "type": "filler"
+                            },
+                            {
+                                "type": "box",
+                                "layout": "vertical",
+                                "contents": [],
+                                "cornerRadius": "30px",
+                                "width": "12px",
+                                "height": "12px",
+                                "borderWidth": "2px",
+                                "borderColor": "#6486E3"
+                            },
+                            {
+                                "type": "filler"
+                            }
+                            ],
+                            "flex": 0
+                        },
+                        {
+                            "type": "text",
+                            "text": "Ochanomizu",
+                            "gravity": "center",
+                            "flex": 4,
+                            "size": "sm"
+                        }
+                        ],
+                        "spacing": "lg",
+                        "cornerRadius": "30px"
+                    },
+                    {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "contents": [
+                        {
+                            "type": "box",
+                            "layout": "baseline",
+                            "contents": [
+                            {
+                                "type": "filler"
+                            }
+                            ],
+                            "flex": 1
+                        },
+                        {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                            {
+                                "type": "box",
+                                "layout": "horizontal",
+                                "contents": [
+                                {
+                                    "type": "filler"
+                                },
+                                {
+                                    "type": "box",
+                                    "layout": "vertical",
+                                    "contents": [],
+                                    "width": "2px",
+                                    "backgroundColor": "#6486E3"
+                                },
+                                {
+                                    "type": "filler"
+                                }
+                                ],
+                                "flex": 1
+                            }
+                            ],
+                            "width": "12px"
+                        },
+                        {
+                            "type": "text",
+                            "text": "Metro 1hr",
+                            "gravity": "center",
+                            "flex": 4,
+                            "size": "xs",
+                            "color": "#8c8c8c"
+                        }
+                        ],
+                        "spacing": "lg",
+                        "height": "64px"
+                    },
+                    {
+                        "type": "box",
+                        "layout": "horizontal",
+                        "contents": [
+                        {
+                            "type": "text",
+                            "text": "20:40",
+                            "gravity": "center",
+                            "size": "sm"
+                        },
+                        {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                            {
+                                "type": "filler"
+                            },
+                            {
+                                "type": "box",
+                                "layout": "vertical",
+                                "contents": [],
+                                "cornerRadius": "30px",
+                                "width": "12px",
+                                "height": "12px",
+                                "borderColor": "#6486E3",
+                                "borderWidth": "2px"
+                            },
+                            {
+                                "type": "filler"
+                            }
+                            ],
+                            "flex": 0
+                        },
+                        {
+                            "type": "text",
+                            "text": "Shinjuku",
+                            "gravity": "center",
+                            "flex": 4,
+                            "size": "sm"
+                        }
+                        ],
+                        "spacing": "lg",
+                        "cornerRadius": "30px"
+                    }
+                    ]
+                }
+                }
+                """
+            message = FlexSendMessage(alt_text="聯絡方式", contents=json.loads(bubble_string))
+            line_bot_api.reply_message(
+                event.reply_token,
+                message
             )
 
 @handler.add(BeaconEvent)
